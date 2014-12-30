f5024d37adac979132fcc734135ea7504957abd7
Bryan Pineda <brynpnd@gmail.com>
2014-12-29 23:03:25 -1000

    Added About title and description

--- app/src/main/java/com/example/bryan/ieee/AboutFragment.java
+++ app/src/main/java/com/example/bryan/ieee/AboutFragment.java
@@ -2,9 +2,11 @@ package com.example.bryan.ieee;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class AboutFragment extends Fragment {
 
@@ -15,6 +17,10 @@ public class AboutFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_about, container, false);
+
+        TextView tv = (TextView) rootView.findViewById(R.id.about_description);
+        tv.setMovementMethod(new ScrollingMovementMethod());
+
 
         return rootView;
     }
